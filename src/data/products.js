aded246f9cd42fd70ac35e8bd769a0c9c34c68fa
Micaela Rosso <micaelad.rosso@gmail.com>
2023-11-21 19:16:47 -0300

    Products

---
+++ src/data/products.js
@@ -0,0 +1,65 @@
+export const products = [ 
+    { 
+        id: 1,
+        title: "PALMERA RAPHIS",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/568/884/products/lucas-plantas00891-8d3713440ec54af2e716483925373019-1024-1024.webp",
+        desc: "Altura: 1,50 M aprox",
+        price: 45000,
+        category: "Exterior",
+    },
+    
+    { 
+        id: 2,
+        title: "CORDATUM (PHILODENDRON HEDERACEUM)",
+        img: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/568/884/products/fotos-plantas-1441-e618dfca0c7760dd5316483229064817-1024-1024.webp",
+        desc: "¡Tamaño grande, igual a las fotos",
+        price: 9000,
+        category: "Exterior",
+    },
+    
+    { 
+        id: 3,
+        title: "MONSTERA ADANSONII",
+        img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/568/884/products/fotos-plantas-1201-d802150c469b2f851916483977147915-1024-1024.webp",
+        desc: "Altura: 45 a 55 cm",
+        price: 18000,
+        category: "Interior",
+    },
+
+    { 
+        id: 4,
+        title: "ARECA FRANCESA",
+        img:"https://d3ugyf2ht6aenh.cloudfront.net/stores/001/568/884/products/234-boho-bajo-arena-planta-2821-4d9066dbe652a30d9716564255771759-1024-1024.webp",
+        desc: "Altura: 1.25m aprox",
+        price: 8500,
+        category: "Interior",
+    },
+    
+    { 
+        id: 5,
+        title: "CITRUS POR LIMON",
+        img: "https://www.concoral.es/tmp/imagenesConcoral/3672115.jpg",
+        desc:"Limonero 4 estaciones",
+        price: 15000,
+        category: "Frutal",
+    },
+
+    { 
+        id: 6,
+        title: "Fantasía",
+        img: "https://pf01.cdn.imgeng.in/foto_producto/48_ramo_rosas_precio_imbatible_san_valentin_.jpg",
+        desc:"24 rosas Premium con Gipsófila, en ramo.",
+        price: 30000,
+        category: "Ramos",
+    },
+    
+    { 
+        id: 7,
+        title: "Encanto",
+        img: "https://pf01.cdn.imgeng.in/foto_producto/1_encanto_000121_720.jpg",
+        desc:"Rosas de tallo largo en jarrón de vidrio.",
+        price: 45000,
+        category: "Ramos",
+    },
+    
+    ]
--- src/screens/home_screen.jsx
+++ src/screens/home_screen.jsx
@@ -1,12 +1,20 @@
 import { CategoryCard } from "../components/categories/category_card/category_card";
 import { CategoryWidget } from "../components/categories/category_widget/category_widget";
 import { Header } from "../components/header/header";
+import { ProductCard } from "../components/products/product_card/product_card";
 
 export const HomeScreen = () => {
     return (
         <div>
         <Header/>
         <CategoryWidget></CategoryWidget>
+        <ProductCard
+        title= "PALMERA RAPHIS"
+        img= "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/568/884/products/lucas-plantas00891-8d3713440ec54af2e716483925373019-1024-1024.webp"
+        desc= "Altura: 1,50 M aprox"
+        price={45000}
+        category= "Exterior"
+        />
 
         </div>
     )
