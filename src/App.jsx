6a08b794e2b65deb4929a24e27de52bb72486d9c
Micaela Rosso <micaelad.rosso@gmail.com>
2023-11-23 14:08:35 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { HomeScreen } from './screens/home_screen'
+import { HomeScreen } from './screens/home/home_screen'
 
 function App() {
   const [count, setCount] = useState(0)
